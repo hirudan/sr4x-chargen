2623ddacfce1bd5095e1c427f2b52ba3e4488651
Andrew Hild <republiqueajamais@hotmail.com>
2019-03-30 14:44:01 -0700

    Added skeletal character component and starting racial data

---
+++ sr4x-chargen/src/character.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// Basic box with increment/decrement arrows that enforces certain rules on
+// allowed values
+class Character extends React.Component {
+
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return();
+  }
+}
+
+export default Character;
