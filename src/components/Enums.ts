738590ac65ae36003fcf80078df1a2e8d2773000
andrewhild <republiqueajamais@hotmail.com>
2020-10-18 22:19:03 -0700

    Start of design

--- src/components/Enums.ts
+++ src/components/Enums.ts
@@ -4,3 +4,11 @@ export enum Attribute{ AGI = "AGI", REA = "REA", STR = "STR", CHA = "CHA", INT =
 RES="RES", BOD = "BOD", ESS = "ESS", INI = "INI", WIL = "WIL" }
 
 export enum KnowSkillCategory{INT, LOG}
+
+export enum AugGrade{Secondhand, Standard, Alpha, Beta, Delta, Cultured}
+
+export enum AugType{Cyberware, Bioware, Nanoware, Geneware, Gear}
+
+export enum AugLoc{Armament, Frame, Technical}
+
+export enum ArmModLoc{Barrel, Under, Top, Integral, None}
---
+++ src/interfaces/Augment.ts
@@ -0,0 +1,19 @@
+﻿import {Attribute, AugGrade, AugType} from "../components/Enums";
+
+export interface Augment{
+    guid? : string,
+    id: number,
+    name: string,
+    desc?: string,
+    type: AugType,
+    grade?: AugGrade,
+    rating?: number,
+    cap?: number,
+    maxRating?: number,
+    costEss: number,
+    costNuyen: number,
+    costCap?: number,
+    relAttrs?: Attribute[],
+    relSkills?: Number[],
+    relTests?: Number[]
+}
