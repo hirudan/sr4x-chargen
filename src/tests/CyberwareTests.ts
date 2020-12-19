8d4a87803b3dfe366a8ff8dae9225016183a2f35
hirudan <republiqueajamais@hotmail.com>
2020-12-18 18:24:19 -0800

    It's a start

---
+++ src/tests/CyberwareTests.ts
@@ -0,0 +1,8 @@
+﻿// import {describe, it} from 'mocha';
+import { expect } from 'chai';
+
+describe("Dummy tests to prove this works", () => {
+   it('should assert true', () => {
+       expect(true).to.equal(true);
+   }) 
+});
--- webpack.config.js
+++ webpack.config.js
@@ -45,5 +45,4 @@ module.exports = [
       extensions: ['.ts', '.tsx', '.js']
     }
   }
-    
 ];
